e2a99390791ada0fd263e50b1bd49ce30e176b72
I&VIEW_Development <xerojin0@naver.com>
2024-10-31 14:52:08 +0900

    조영진
    2024.10.31 오전 커밋

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  var swiper = new Swiper(".portfolio_list", {
+    spaceBetween: 20,
+    slidesPerView: 3.2,
+    loop: false,
+    loopAdditionalSlides: 1,
+  });
+});

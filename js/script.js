f83b9ef43bdc7d695677cdbb3f6e7df2cc169988
I&VIEW_Development <xerojin0@naver.com>
2024-11-06 17:39:45 +0900

    조영진
    PC/모바일 퍼블리싱 완료

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 $(document).ready(function () {
   var swiper = new Swiper(".swiper_block01", {
     spaceBetween: 20,
-    slidesPerView: 3.2,
+    slidesPerView: 1,
+    centeredSlides: true,
     loop: false,
     loopAdditionalSlides: 1,
+    breakpoints: {
+      768: {
+        slidesPerView: 2.2,
+        spaceBetween: 20,
+        centeredSlides: false,
+        slidesOffsetBefore: 20,
+        slidesOffsetAfter: 20,
+      },
+      1024: {
+        slidesPerView: 2.8,
+        spaceBetween: 20,
+        centeredSlides: false,
+        slidesOffsetBefore: 20,
+        slidesOffsetAfter: 20,
+      },
+      1400: {
+        slidesPerView: 3.2,
+        spaceBetween: 20,
+        centeredSlides: false,
+        slidesOffsetBefore: 20,
+        slidesOffsetAfter: 20,
+      },
+    },
   });
-
 });
